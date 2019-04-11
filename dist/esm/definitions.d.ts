dd2f40d5306240089e2add6314d1e88736b6da6c
Florian Strauß <fs@orlyapps.de>
2019-04-11 21:45:53 +0200

    “wip”

--- dist/esm/definitions.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-    interface PluginRegistry {
-        CameraOverlay?: CameraOverlayPlugin;
-    }
-}
-export interface CameraOverlayPlugin {
-    startCamera(): Promise<void>;
-    stopCamera(): Promise<void>;
-}
--- dist/esm/web.d.ts
+++
@@ -1,9 +0,0 @@
-import { WebPlugin } from "@capacitor/core";
-import { CameraOverlayPlugin } from "./definitions";
-export declare class CameraOverlayWeb extends WebPlugin implements CameraOverlayPlugin {
-    constructor();
-    startCamera(): Promise<void>;
-    stopCamera(): Promise<void>;
-}
-declare const CameraOverlay: CameraOverlayWeb;
-export { CameraOverlay };
